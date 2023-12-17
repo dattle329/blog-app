2087bf74c0f53cbbb5f2fb96b2de78d4ebfaa3d0
Le Tuan Dat <dattle329@gmail.com>
2023-12-17 11:00:05 +0700

    [ADD] Post mapper

---
+++ src/main/java/com/vti/demorail79app/mapper/PostMapper.java
@@ -0,0 +1,25 @@
+package com.vti.demorail79app.mapper;
+
+import com.vti.demorail79app.dto.PostDto;
+import com.vti.demorail79app.entity.Post;
+import com.vti.demorail79app.form.PostCreateForm;
+
+public class PostMapper {
+    public static Post map(PostCreateForm form){
+        var post = new Post();
+        post.setTitle(form.getTitle());
+        post.setContent(form.getContent());
+        post.setDescription(form.getDescription());
+        return post;
+    }
+    public static PostDto map(Post post){
+        var dto = new PostDto();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setDescription(post.getDescription());
+        dto.setContent(post.getContent());
+        dto.setCreatedAt(post.getCreatedAt());
+        dto.setUpdatedAt(post.getUpdatedAt());
+        return dto;
+    }
+}
