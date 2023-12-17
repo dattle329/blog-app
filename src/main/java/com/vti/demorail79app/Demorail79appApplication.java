6e1073da64c3a87b216d7ef7eaa35e3af3cc8032
Le Tuan Dat <dattle329@gmail.com>
2023-12-17 09:41:49 +0700

    [ADD] rail79app

---
+++ src/main/java/com/vti/demorail79app/Demorail79appApplication.java
@@ -0,0 +1,13 @@
+package com.vti.demorail79app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demorail79appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demorail79appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/demorail79app/Demorail79appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.demorail79app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demorail79appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
