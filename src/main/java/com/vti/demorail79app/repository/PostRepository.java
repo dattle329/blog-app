f2bf1f012304e0201e1eebfa4e4d6f970272c94b
Le Tuan Dat <dattle329@gmail.com>
2023-12-17 10:09:59 +0700

    [ADD] Post repository

---
+++ src/main/java/com/vti/demorail79app/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.vti.demorail79app.repository;
+
+import com.vti.demorail79app.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
