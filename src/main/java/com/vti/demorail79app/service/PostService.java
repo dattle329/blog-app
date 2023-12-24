5fea857c200f5f8ef7a7a27afb39fe257c361032
Le Tuan Dat <dattle329@gmail.com>
2023-12-24 10:07:10 +0700

    [Add] User entity

--- src/main/java/com/vti/demorail79app/service/PostService.java
+++ src/main/java/com/vti/demorail79app/service/PostService.java
@@ -1,14 +1,11 @@
 package com.vti.demorail79app.service;
 
 import com.vti.demorail79app.dto.PostDto;
-import com.vti.demorail79app.entity.Post;
 import com.vti.demorail79app.form.PostCreateForm;
 import com.vti.demorail79app.form.PostFilterForm;
 import com.vti.demorail79app.form.PostUpdateForm;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.Optional;
 
 
 public interface PostService {
