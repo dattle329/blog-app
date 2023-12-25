7e0b1e8fb3c18ae04d7952b1874b11a8e236ffc0
Le Tuan Dat <dattle329@gmail.com>
2023-12-25 09:49:17 +0700

    [Add] Encode user password

--- src/main/java/com/vti/demorail79app/service/UserServiceImpl.java
+++ src/main/java/com/vti/demorail79app/service/UserServiceImpl.java
@@ -5,16 +5,20 @@ import com.vti.demorail79app.form.UserCreateForm;
 import com.vti.demorail79app.mapper.UserMapper;
 import com.vti.demorail79app.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDto create(UserCreateForm form) {
         var user = UserMapper.map(form);
+        var encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         var savedUser = userRepository.save(user);
         return UserMapper.map(savedUser);
     }
